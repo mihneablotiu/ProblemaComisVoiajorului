6109c3f77005982ebaa31b8d8ada263f9d87f291
Mihnea-Andrei BLOȚIU <mihnea.blotiu@stud.acs.upb.ro>
2024-01-08 14:22:47 +0200

    Pthread times

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 
 make build
 
-THREADS=2
+THREADS=24
+PROCS=2
 
 # for i in {1..14}; do
 #    total_time=0.0
@@ -31,59 +32,59 @@ THREADS=2
 #    echo "Test ${i} done - serial"
 # done
 
-for i in {1..14}; do
-    total_time=0.0
-    last_path=""
-    last_minimum_cost=""
-
-    for j in {1..3}; do
-        output=$(./openMP_implementation ${THREADS} < tests/in/test${i}.in)
-    
-        elapsed_time=$(echo "$output" | awk '/Total time:/ { print $3 }')
-        last_path=$(echo "$output" | awk '/Path:/ { $1=""; print $0 }')
-        last_minimum_cost=$(echo "$output" | awk '/Minimum cost:/ { print $3 }')
-
-        total_time=$(echo "$total_time + $elapsed_time" | bc)
-    done
-
-    mean_total_time=$(echo "scale=6; $total_time / 3" | bc)
-
-    if [[ "$mean_total_time" == .* ]]; then
-        mean_total_time="0$mean_total_time"
-    fi
-
-    echo "Path: ${last_path}" > tests/out_openMP/test${i}.out.${THREADS}.average
-    echo "Minimum cost: ${last_minimum_cost}" >> tests/out_openMP/test${i}.out.${THREADS}.average
-    echo "Mean total time: ${mean_total_time} seconds" >> tests/out_openMP/test${i}.out.${THREADS}.average
-    echo "Test ${i} done - openMP"
-done
-
-# for i in {1..15}; do
+# for i in {1..14}; do
 #     total_time=0.0
 #     last_path=""
 #     last_minimum_cost=""
 
-#     for j in {1..10}; do
-#         output=$(./pthread_implementation < tests/in/test${i}.in)
-        
+#     for j in {1..3}; do
+#         output=$(./openMP_implementation ${THREADS} < tests/in/test${i}.in)
+    
 #         elapsed_time=$(echo "$output" | awk '/Total time:/ { print $3 }')
 #         last_path=$(echo "$output" | awk '/Path:/ { $1=""; print $0 }')
 #         last_minimum_cost=$(echo "$output" | awk '/Minimum cost:/ { print $3 }')
 
 #         total_time=$(echo "$total_time + $elapsed_time" | bc)
 #     done
-    
-#     mean_total_time=$(echo "scale=6; $total_time / 20" | bc)
-    
+
+#     mean_total_time=$(echo "scale=6; $total_time / 3" | bc)
+
 #     if [[ "$mean_total_time" == .* ]]; then
 #         mean_total_time="0$mean_total_time"
 #     fi
-    
-#     echo "Path: ${last_path}" > tests/out_pthreads/test${i}.out
-#     echo "Minimum cost: ${last_minimum_cost}" >> tests/out_pthreads/test${i}.out
-#     echo "Mean total time: ${mean_total_time} seconds" >> tests/out_pthreads/test${i}.out
-#     echo "Test ${i} done - pthreads"
+
+#     echo "Path: ${last_path}" > tests/out_openMP/test${i}.out.${THREADS}.average
+#     echo "Minimum cost: ${last_minimum_cost}" >> tests/out_openMP/test${i}.out.${THREADS}.average
+#     echo "Mean total time: ${mean_total_time} seconds" >> tests/out_openMP/test${i}.out.${THREADS}.average
+#     echo "Test ${i} done - openMP"
 # done
+
+for i in {1..14}; do
+    total_time=0.0
+    last_path=""
+    last_minimum_cost=""
+
+    for j in {1..3}; do
+        output=$(./pthread_implementation ${THREADS} < tests/in/test${i}.in)
+        
+        elapsed_time=$(echo "$output" | awk '/Total time:/ { print $3 }')
+        last_path=$(echo "$output" | awk '/Path:/ { $1=""; print $0 }')
+        last_minimum_cost=$(echo "$output" | awk '/Minimum cost:/ { print $3 }')
+
+        total_time=$(echo "$total_time + $elapsed_time" | bc)
+    done
+    
+    mean_total_time=$(echo "scale=6; $total_time / 3" | bc)
+    
+    if [[ "$mean_total_time" == .* ]]; then
+        mean_total_time="0$mean_total_time"
+    fi
+    
+    echo "Path: ${last_path}" > tests/out_pthreads/test${i}.out.${THREADS}.average
+    echo "Minimum cost: ${last_minimum_cost}" >> tests/out_pthreads/test${i}.out.${THREADS}.average
+    echo "Mean total time: ${mean_total_time} seconds" >> tests/out_pthreads/test${i}.out.${THREADS}.average
+    echo "Test ${i} done - pthreads"
+done
 
 # for i in {1..15}; do
 #     total_time=0.0
